2ed4f64498e4b352789fb7d17fce8e71b487c716
imamfarisi19 <edwardindo1@gmail.com>
2020-12-27 12:09:28 +0800

    make ContohCtroller.php file in App\Http\Controllers directory

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+}); 
 
 Route::get('/halaman-satu', function(){
     return view('halaman-satu');
