83058487785b449d5cf77a7d2b0783c3538eb56a
imamfarisi19 <edwardindo1@gmail.com>
2020-12-27 23:05:52 +0800

    register index function in routes

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/', function () {
 Route::get('halaman-satu',[ContohController::class,'index']);
 Route::get('create-mahasiswa',[MahasiswaController::class,'create'])->name('create-mahasiswa');
 Route::post('simpan-mahasiswa',[MahasiswaController::class,'store'])->name('simpan-mahasiswa');
+Route::get('data-mahasiswa',[MahasiswaController::class,'index'])->name('data-mahasiswa');
