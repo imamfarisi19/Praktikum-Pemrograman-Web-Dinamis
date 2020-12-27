8451c701e591a987c6a4287887f161cf9d0ce886
imamfarisi19 <edwardindo1@gmail.com>
2020-12-27 15:08:36 +0800

    for input data mahasiswa

---
+++ resources/views/Create-mahasiswa.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>Tambah Data Mahasiswa</div>
+    <div>
+        <form action="#" method="POST">
+        {{ csrf_field() }}
+        <div>
+            <input type="text" id="nama" name="nama" placeholder="Nama Mahasiswa">
+        </div>
+        <div>
+            <input type="text" id="alamat" name="alamat" placeholder="Alama Mahasiswa">
+        </div>
+        <div>
+            <button type="submit">Simpan</button>
+        </div>
+    </div>
+</body>
+</html>
--- resources/views/halaman-satu.blade.php
+++ resources/views/halaman-satu.blade.php
@@ -1,3 +1,3 @@
 <h1>Halaman Satu</h1>
 <br>
-<h1>Hey Nama Saya Adalah{{ $varNama}}</h1>
+<h1>Hey Nama Saya Adalah {{ $varNama}}</h1>
