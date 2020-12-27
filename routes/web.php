eed4e309e85ac38b15123c5d505af61918626e9a
imamfarisi19 <edwardindo1@gmail.com>
2020-12-27 16:43:23 +0800

    add use App\Http\Controllers\MahasiswaController in top

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContohController;
+use App\Http\Controllers\MahasiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,4 @@ Route::get('/', function () {
 }); 
 
 Route::get('halaman-satu',[ContohController::class,'index']);
-    
+Route::get('create-mahasiswa',[MahasiswaController::class,'create'])->name('create-mahasiswa');
