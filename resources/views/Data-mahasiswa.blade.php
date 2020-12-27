4649e326be40e7d7e7aa7229776da03fff2b0060
imamfarisi19 <edwardindo1@gmail.com>
2020-12-27 22:58:20 +0800

    source code for view Data Mahasiswa in browser

---
+++ resources/views/Data-mahasiswa.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h4>Data Mahasiswa</h4>
+    </div>
+    <div><a href="">Tambah Data</a></div>
+    <div>
+        <table border="1">
+            <tr>
+                <th style="1%">#</th>
+                <th style="30%">Nama</th>
+                <th style="50%">Alamat</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
