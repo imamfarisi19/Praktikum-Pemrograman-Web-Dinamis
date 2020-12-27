a3f53281c057718f70cf9fcfc4ce165b675bd936
imamfarisi19 <edwardindo1@gmail.com>
2020-12-28 00:29:24 +0800

    add new page edit-mahasiswa.blade.php

---
+++ resources/views/Edit-mahasiswa.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>Ubah Data Mahasiswa</div>
+    <div>
+        <form action="#" method="POST">
+            {{ csrf_field() }}
+            <div>
+                <input type="text" id="nama" name="nama" placeholder="Nama Mahasiswa">
+            </div>
+            <div>
+                <input type="text" id="alamat" name="alamat" placeholder="Alamat Mahasiswa">
+            </div>
+            <div>
+                <button type="submit">Ubah</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
